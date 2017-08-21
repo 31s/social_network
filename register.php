2a5ef99111f701d9b26c1f8b3c152b6061e19092
ibrahim <duranteau.l.i@gmail.com>
2017-08-21 19:50:35 +0100

    phpmailer

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 
 <?php
 
+require_once('config/PHPMailer-master/PHPMailerAutoload.php');
 require('config/database.php');
 require('includes/function.php');
 require('includes/constants.php');
@@ -53,10 +54,39 @@ require('includes/constants.php');
                 $headers = 'MIME-Version: 1.0' . "\r\n";
                 $headers = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-                mail($to, $subject, $content, $headers);
+                //mail($to, $subject, $content, $headers);
 
                 // Informer utilisateur pour verifier mail
                 echo "Mail d'activation envoyé !";
+
+                function smtpmailer($to, $from, $from_name, $subject, $body) { 
+                    global $error;
+                    $mail = new PHPMailer();  // create a new object
+                    $mail->IsSMTP(); // enable SMTP
+                    $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+                    $mail->SMTPAuth = true;  // authentication enabled
+                    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->Port = 465; 
+                    $mail->Username = '31s.duranteau@gmail.com';  
+                    $mail->Password = 'idcommunication';           
+                    $mail->SetFrom($from, $from_name);
+                    $mail->Subject = $subject;
+                    $mail->Body = $body;
+                    $mail->IsHTML(true); 
+                    $mail->AddAddress($to);
+                    if(!$mail->Send()) {
+                        $error = 'Mail error: '.$mail->ErrorInfo; 
+                        return false;
+                    } else {
+                        $error = 'Message sent!';
+                        return true;
+                    }
+                }
+
+                 	
+                smtpmailer($email, 'from@mail.com', 'Social Network', $subject, $content);
+
             }
 
         } else {

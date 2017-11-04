2bb2ae04a4caef81dfd3b15069c907aef83e0d8c
ibrahim <duranteau.l.i@gmail.com>
2017-11-04 16:40:10 +0000

    i18n

---
+++ locales/long_text.php
@@ -0,0 +1,12 @@
+<?php
+
+    $long_text = [
+        "accueil_intro" => [
+            'fr' => "est le réseau social des développeurs. ⌨ <br/>
+            Et qui dit développeurs, dit également code source ! ↩<br/>
+            Grace à cette plateforme, vous aurez la possibilité de tisser des liens d'amitiés avec d'autres développeurs, échanger des codes sources, recevoir de l'aide si vous rencontrez des problèmes sur l'un de vos projets, et bien plus encore ! <br>
+            alors n'hésitez plus et <a href='register.php'>rejoignez dès maintenant la communauté</a> !</p>
+            <a href='register.php' class='btn btn-primary btn-lg'>Créer un compte</a>",
+
+            'en' => "Long text in english"]
+    ];
---
+++ locales/menu.php
@@ -0,0 +1,10 @@
+<?php
+
+    $menu = [
+        "accueil" => ['fr' => 'Accueil', 'en' => 'Home'],
+        "connexion" => ['fr' => 'Connexion', 'en' => 'Login'],
+        "inscription" => ['fr' => 'Inscription', 'en' => 'Register'],
+        "mon_profil" => ['fr' => 'Mon profil', 'en' => 'My account'],
+        "share_code" => ['fr' => 'Partager', 'en' => 'Share'],
+        "deconnexion" => ['fr' => 'Deconnexion', 'en' => 'Logout']
+    ];
